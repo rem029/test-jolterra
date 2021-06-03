082cf1588316485baa27104632b4ee2e8107e05b
rem029 <elawrenceponce@gmail.com>
2021-06-03 09:00:29 +0300

    Footer.js Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import '../styles/footer.css';
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div>Logo</div>
       <div>
-        <a href="#" >Links</a>
-        <a href="#" >Links</a>
-        <a href="#" >Links</a>
+        <a href="http://rem029.github.io/test-jolterra">Links</a>
+        <a href="http://rem029.github.io/test-jolterra">Links</a>
+        <a href="http://rem029.github.io/test-jolterra">Links</a>
       </div>
       <div>icons</div>
       <div>copyright</div>
