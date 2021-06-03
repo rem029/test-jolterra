2149f77d2bdf812a9b799960fd5aba851c12ec57
rem029 <elawrenceponce@gmail.com>
2021-06-03 09:42:59 +0300

    updated title and description.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
+import { HiOutlineChartBar } from "react-icons/hi";
+
 import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div>Logo</div>
-      <div>
+      <div className="footer__logo">
+        <HiOutlineChartBar />
+        <h1>Jolterra</h1>
+      </div>
+      <div className="footer__links">
         <a href="http://rem029.github.io/test-jolterra">Links</a>
         <a href="http://rem029.github.io/test-jolterra">Links</a>
         <a href="http://rem029.github.io/test-jolterra">Links</a>
       </div>
-      <div>icons</div>
-      <div>copyright</div>
+      <div className="footer__icons">icons</div>
+      <div className="footer__copyright">copyright</div>
     </div>
   );
 };
